c3da39a0a6d3eb75f085f335985450a392ecfd83
masrapi31 <raficiaa@gmail.com>
2024-09-18 22:31:04 +0700

    230441100026_Ar'raffiAbqoriNurAzizi_KakIqbal_Pengumpulan-Modul-2 (#2)
    * pengumpulan modul 1 kak hehehe
    
    * latihan
    
    * pengumpulan latihan

---
+++ modul1-latihan/coba.py
@@ -0,0 +1,12 @@
+# Operator Hitung (Penggunaan Tipe Data)
+# Menggunakan tipe data untuk menghitung hasil akhir
+jumlah_barang = 5
+harga_per_barang = 10000
+
+total_harga = jumlah_barang * harga_per_barang
+print(f"Total Harga: {total_harga}")
+
+# Menggunakan operator untuk menghitung diskon
+diskon = 0.1  # 10% diskon
+harga_setelah_diskon = total_harga - (total_harga * diskon)
+print(f"Harga Setelah Diskon: {harga_setelah_diskon}")
